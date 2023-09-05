7452f81dbc38344ab14d3d9c1874049686abcaa4
fan.tai <>
2023-09-05 12:54:48 +0800

    node.js新增ejs, ejs-locals, nodemon index使用按鈕post數據

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
 const express = require('express');
 const app = express();
 
-// app.get('/', (request, response) => {
-//     response.send(`<h1>Hello, Node</h1>`)
-// })
+// 将静态文件放在名为 "resources" 的目录中
+app.use(express.static('resources'));
 
-// app.get("/page/index", (request, response) => {
-//     response.send(`<h1>Hello, Node</h1>`);
-// });
+// 用于处理HTTP POST请求中的表单数据。具体来说，它处理了以下内容
+app.use(express.urlencoded({
+    /*
+    处理表单数据的扩展选项：在代码中，extended: false 选项告诉中间件使用Node.js内置的querystring库来解析表单数据，而不是使用更复杂的第三方库。
+    这意味着解析的数据将不包含嵌套对象和数组。如果将此选项更改为true，则中间件将使用第三方库来支持更复杂的数据结构。
+    */
+    extended: false
+}))
 
-// app.get("/page/about", (request, response) => {
-//     response.send(`<h1>About Node</h1>`);
-// });
-
-// app.use((req, res, next) => {
-//     console.log("這是 middleware");
-//     // notDefined(); // 執行一個沒有定義的函式
-//     next(); // 一定要加這個函式才會往下處理
-// });
-app.use(express.static('resources')); // 将静态文件放在名为 "resources" 的目录中
+// EJS 核心
+const engine = require("ejs-locals");
+app.engine("ejs", engine);
+// 讀取 EJS 檔案位置
+app.set('views', './views');
+// 用 EJS 引擎跑模板
+app.set("view engine", "ejs");
 
 function middleware(req, res, next) {
     console.log("這是 middleware");
@@ -27,7 +28,11 @@ function middleware(req, res, next) {
     if (firstPathSegment === 'page') {
         next();
     } else {
-        if(firstPathSegment == "") res.send("<h1>這是 middleware 練習</h1><img src='./images/haha.png'>");
+        if (firstPathSegment == "") {
+            // res.send("<h1>這是 middleware 練習</h1><img src='./images/haha.png'>");
+            res.render("index");
+
+        }
         else res.status(404).send("404 Oops! 找不到網頁！");
     }
 
@@ -93,6 +98,17 @@ app.use((err, req, res, next) => {
     console.log("err status=", err.status);
     res.status(500).send("500 程式錯誤，請聯繫 IT 人員協助！");
 });
+
+app.post('/answer', function (req, res) {
+    console.log("post =", req.body);
+    console.log("post =", req.body.preferColor);
+    if (req.body.preferColor == "紅色" || req.body.preferColor == "red") {
+        res.send("答對了，你真暸解我")
+    } else {
+        res.send("對不起，不是這個答案")
+    }
+    // res.send("感謝送出表單")
+})
 
 // 監聽 port
 
